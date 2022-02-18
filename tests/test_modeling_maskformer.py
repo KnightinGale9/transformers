9196d0a5538585d42c0ca27a60048bec039c0f9f
fra <francesco.zuppichini@gmail.com>
2022-02-18 18:14:07 +0100

    torch_device

--- tests/test_modeling_maskformer.py
+++ tests/test_modeling_maskformer.py
@@ -20,9 +20,9 @@ import unittest
 import numpy as np
 
 from tests.test_modeling_common import floats_tensor
-from transformers import MaskFormerConfig, is_torch_available, is_vision_available, torch_device
+from transformers import MaskFormerConfig, is_torch_available, is_vision_available
 from transformers.file_utils import cached_property
-from transformers.testing_utils import require_torch, require_vision, slow
+from transformers.testing_utils import require_torch, require_vision, slow, torch_device
 
 from .test_configuration_common import ConfigTester
 from .test_modeling_common import ModelTesterMixin
